d9e6a87f2dad8ce450d7dca092e66b84121da8d3
Pankaj <pankaj4u4m@gmail.com>
2026-08-02 14:44:26 +0200

    chore: brand host build output

--- packages/script/src/index.ts
+++ packages/script/src/index.ts
@@ -74,4 +74,4 @@ export const Script = {
     return team
   },
 }
-console.log(`opencode script`, JSON.stringify(Script, null, 2))
+console.log("lemoncode build", JSON.stringify({ ...Script, team: undefined }, null, 2))
